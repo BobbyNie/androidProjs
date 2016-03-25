b9e3172867021ac00e732c752853fe66c69dcb5d
kfzx-niebo <niebo@sdc.icbc.com.cn>
2016-03-25 13:51:36 +0800

    修改来电监听方式 满足三星手机双卡双待

--- FirstSms/src/com/bobby/firstsms/CallReceiver.java
+++ FirstSms/src/com/bobby/firstsms/CallReceiver.java
@@ -6,93 +6,47 @@ import java.util.Locale;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-import android.app.Service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.telephony.TelephonyManager;
+import android.os.Bundle;
 
 public class CallReceiver extends BroadcastReceiver {
-	private static boolean incomingFlag = false;
+	//private static boolean incomingFlag = false;
 
 	private static final SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.CHINA);
 
 	// private String incomingNumber;
 	@Override
 	public void onReceive(Context context, Intent intent) {
-
-		// System.out.println( intent.get());
-		// 拨打电话
-		if (intent.getAction().equals(Intent.ACTION_NEW_OUTGOING_CALL)) {
-			incomingFlag = false;
-			// final String phoneNum =
-			// intent.getStringExtra(Intent.EXTRA_PHONE_NUMBER);
-			// Log.d("PhoneReceiver", "phoneNum: " + phoneNum);
-		} else {
-			TelephonyManager tm = (TelephonyManager) context.getSystemService(Service.TELEPHONY_SERVICE);
 			// 获取卡来源 -- 天语手机
-			final int subscription = intent.getIntExtra("subscription", 0);
-			final int appId = (subscription == 0 ? 6 : 5);
-			final String phone = (subscription == 0 ? "186":"134");
-			
-			final String incomingNumber = intent.getStringExtra("incoming_number");
-			switch (tm.getCallState()) {
-			case TelephonyManager.CALL_STATE_RINGING:
-				incomingFlag = true;// 标识当前是来电
-				pool.execute(new Runnable() {
-					@Override
-					public void run() {
-						try {
-							// 134手机插入卡槽1 186手机插入插槽2 微信企业号 134手机id为 5
-							// 186手机id为 6
-							WeChartSender.sendWeChart(appId, format.format(new Timestamp(System.currentTimeMillis())) + ":\n" + incomingNumber + "  正在拨打"+phone);
-						} catch (Exception e) {
-							// do nothing
-						}
-					}
-				});
-				break;
-
-			case TelephonyManager.CALL_STATE_OFFHOOK:
-				if (incomingFlag) {
-					pool.execute(new Runnable() {
-						@Override
-						public void run() {
-							try {
-								// 134手机插入卡槽1 186手机插入插槽2 微信企业号 134手机id为 5
-								// 186手机id为 6
-								WeChartSender.sendWeChart(appId, format.format(new Timestamp(System.currentTimeMillis())) + ":\n" + incomingNumber + "  接通电话");
-							} catch (Exception e) {
-								// do nothing
-							}
-						}
-					});
-
+		final int subscription = intent.getIntExtra("subscription", 0);
+		final int appId = (subscription == 0 ? 6 : 5);
+		final String phone = (subscription == 0 ? "186":"134");
+		//String telephonyServiceName = (subscription == 0 ? "phone":"phone_msim");
+		
+		//TelephonyManager tm = (TelephonyManager) context.getSystemService(Service.TELEPHONY_SERVICE);
+		final String incomingNumber = intent.getStringExtra("incoming_number");
+		
+		 // 如果是来电
+        Bundle bundler = intent.getExtras();
+        // 可以Debug看下这个bundler里面的数据，就明白为什么我找到了
+        final String ring_state = bundler.getString("state");
+        //String number = bundler.getString("incoming_number");
+        
+    	pool.execute(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					// 134手机插入卡槽1 186手机插入插槽2 微信企业号 134手机id为 5
+					// 186手机id为 6
+					WeChartSender.sendWeChart(appId, format.format(new Timestamp(System.currentTimeMillis())) + ":\n" + incomingNumber+" " +ring_state + " on "+phone);
+				} catch (Exception e) {
+					// do nothing
 				}
-				break;
-
-			case TelephonyManager.CALL_STATE_IDLE:
-				if (incomingFlag) {
-					pool.execute(new Runnable() {
-						@Override
-						public void run() {
-							try {
-								// 134手机插入卡槽1 186手机插入插槽2 微信企业号 134手机id为 5
-								// 186手机id为 6
-								WeChartSender.sendWeChart(appId, format.format(new Timestamp(System.currentTimeMillis())) + ":\n" + incomingNumber + "  挂机");
-							} catch (Exception e) {
-								// do nothing
-							}
-						}
-					});
-
-				}
-
-				break;
-
 			}
+		}); 
 
-		}
 	}
 	ExecutorService pool = Executors.newSingleThreadExecutor();
 }
